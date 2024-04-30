f41478f9d88bc259ad83a4827019d510631858d6
Ann He <annxhe@gmail.com>
2024-04-30 14:42:57 +0100

    add expansion function

--- ai_planning_searching/mcts.py
+++ ai_planning_searching/mcts.py
@@ -5,7 +5,7 @@ import json
 from transformers import pipeline, set_seed
 
 class Node:
-	token: str
+	token: str # we assume that the token is the concatenation of all generated on this path
 	value: float
 	Q_s_a: dict[str,float]
 	P_UCB_s_a: dict[str, float]
@@ -27,6 +27,35 @@ def select(root:Node) -> Node:
 		arg_max, max_UCB = max(list(enumerate(UCB_values)), key=lambda x: x) 
 		root = root.children[arg_max]
 	return root
+
+
+def expand(root:Node, model, k, max_beam_len):
+	# get top k
+	beam_output = model.generate(
+    root.tokens,
+    max_new_tokens=40,
+    num_beams=k,
+    early_stopping=True)
+
+    beams = [tokenizer.decode(beam_k, skip_special_tokens=True) for beam_k in beam_output]
+    ## roll this out
+    beam_list = [root.tokens + beam for beam in beams]
+    for _ in range(max_beam_len-1):
+    	new_list = []
+    	for current_path in beam_list:
+    		beam_output = model.generate(
+    		current_path,
+    		max_new_tokens=40,
+    		num_beams=k,
+    		early_stopping=True)
+
+    		beams = [tokenizer.decode(beam_k, skip_special_tokens=True) for beam_k in beam_output]
+    		new_list.extend([current_path + beam for beam in beams])
+    	beam_list = new_list
+    return beam_list # should be list of k^(max_beam_len) full paths
+
+
+
 
 
 
