6e74be53221b06d35857e45d576402aaa25d101d
Ann He <annxhe@gmail.com>
2024-05-06 15:13:22 +0100

    fleshed out mcts_utils

---
+++ ai_planning_searching/mcts_utils.py
@@ -0,0 +1,119 @@
+import numpy as np
+import torch
+import collections
+import json
+from transformers import pipeline, set_seed
+
+tokenizer = transformers.AutoTokenizer.from_pretrained('gpt2')
+lm = transformers.AutoModelForCausalLM.from_pretrained('gpt2')
+
+class Node:
+	tokens: torch.Tensor # we assume that the token is the concatenation of all generated on this path
+	string: str
+	Q_s_a: dict[str,float]
+	P_UCB_s_a: dict[str, float]
+	P_s_a : torch.Tensor # keep the logits
+	visits: int
+	children: list[Node]
+
+	def __init__(self, token=token, value=0, visits=0):
+		self.token = token
+		self.value = value
+		self.visits = visits
+		self.Q_s_a = {}
+		self.P_UCB_s_a = {}
+		self.children = []
+
+
+def select(root:Node) -> Node:
+	path_nodes = []
+	path_nodes.append(root)
+	while len(root.children) > 0:
+		UCB_values = [P_UCB_s_a[child.str] for child in root.children]
+		arg_max, max_UCB = max(list(enumerate(UCB_values)), key=lambda x: x) 
+		root = root.children[arg_max]
+		path_nodes.append(root)
+	return root, path_nodes
+
+
+def expand(root:Node, tokenizer, model, k, max_beam_len):
+	"""
+	Note that we can do a more fine-grained exploration of expand by interpolating
+	between the expansion and the roll-out, both of which are done with top-k decoding / beam search.
+	"""
+	# get top k
+	beam_output = model.generate(
+    root.tokens,
+    max_new_tokens=1,
+    num_beams=k,
+    num_return_sequences=k,
+    return_dict_in_generate=True,
+    output_scores=True,
+    early_stopping=True)
+
+    # todo(annhe): Add scores of beams to node.P_s_a
+    # https://discuss.huggingface.co/t/how-to-get-sequences-scores-from-scores-in-generate-method/6048
+    # print(beam_output['scores'][0].size())
+    root.P_s_a = beam_output.sequences_scores # size 
+
+    # you have to do some chunking here
+    scores = list(torch.chunk(beam_output.sequences_scores,chunks=k,dim=0))
+    beams = list(torch.chunk(beam_output.sequences,chunks=k,dim=0))
+    beam_list = [(s,b) for s,b in zip(scores,beams)]
+
+    for _ in range(max_beam_len-1):
+    	new_list = []
+    	for current_path in beam_list:
+    		beam_output = model.generate(
+    		current_path,
+    		max_new_tokens=1,
+    		num_beams=k,
+    		num_return_sequences=k,
+    		return_dict_in_generate=True,
+    		output_scores=True,
+    		early_stopping=True)
+    		current_beams = list(torch.chunk(beam_output.sequences,chunks=k,dim=0))
+    		scores = list(torch.chunk(beam_output.sequences_scores,chunks=k,dim=0))
+    		current_beam_list = [(s,b) for s,b in zip(scores,current_beams)]
+
+    		new_list.extend(current_beam_list)
+
+    	beam_list = new_list
+    # should be list of k^(max_beam_len) full paths
+    return beam_list 
+
+
+def evaluate_full_paths(beam_list: list[tuple[torch.Tensor, torch.Tensor]]): 
+	# returns full decoded path and its score
+	# consider using a different reward model
+	# suggested in https://openreview.net/pdf?id=Lr8cOOtYbfL to use max(score)
+	res = sorted(beam_list,key=lambda x: x[1], reverse=True)[0]
+	return res
+
+
+
+def backpropagate_statistics(path_nodes, max_rollout_reward, c_base, c):
+	# 1. add 1 to all state visit counts
+	# 2. recalculate P_UCB_s_a recursively (backwards)
+	# 3. update Q_s_a with max_rollout_reward
+	# According to the paper, this is
+	# Q(s'',a'') <-- max(Q(s'',a''),r)
+	# P(a|s) given by last log softmax in the return value
+	
+	# We need to update path_nodes in reverse order
+	reversed_path_nodes = list(reversed(path_nodes))
+	for i, node in enumerate(reversed_path_nodes):
+		node.visits += 1
+		# Q(s'',a'') <-- max(Q(s'',a''),r)
+		new_q_s_a = {k: max(v, max_rollout_reward) for k,v in node.Q_s_a.items()}
+		node.Q_s_a = new_q_s_a
+		# Recalculated P_UCB_s_a
+		beta = math.log((node.visits + c_base + 1) / c_base) + c
+		if i > 0:
+			s_prime_visits = reversed_path_nodes[i-1].visits 
+		else:
+			s_prime_visits = 0
+		for i, (k, v) in enumerate(new_q_s_a.items()):
+    		print(i, k, v)
+    		node.P_UCB_s_a[k] = v + beta * node.P_s_a[i] * math.sqrt(torch.log(node.visits)) / (1 + s_prime_visits)
+
