f0900d0671f12645e77f8f975fc26cd858a712a6
Ann He <annxhe@gmail.com>
2024-07-29 11:19:14 -0700

    adding test suite

--- ai_planning_searching/mcts_utils.py
+++ ai_planning_searching/mcts_utils.py
@@ -3,6 +3,7 @@ import torch
 import collections
 import json
 from transformers import pipeline, set_seed
+import transformers
 
 tokenizer = transformers.AutoTokenizer.from_pretrained('gpt2')
 lm = transformers.AutoModelForCausalLM.from_pretrained('gpt2')
@@ -26,23 +27,23 @@ TODO7(annhe):
 This should be refactored / cleaned up.
 """
 class Node:
-	# todo(annhe): refactor this so that the node is just a single token
-	current_token: torch.Tensor
-	sequence_tokens: torch.Tensor # we assume that the token is the concatenation of all generated on this path
-	string: str # string representation of current token
-	sequence_string: str # string representation of current sequence
-	Q_s_a: dict[str,float] # Q values of the node's children
-	P_UCB_s_a: dict[str, float] # P-UCB values of the node's children
-	P_s_a : torch.Tensor # keep the logits of the node's children
-	visits: int
-	children: list[Node]
+    # todo(annhe): refactor this so that the node is just a single token
+    current_token: torch.Tensor
+    string: str # string representation of current token
+    Q_s_a: dict[str,float] # Q values of the node's children
+    P_UCB_s_a: dict[str, float] # P-UCB values of the node's children
+    P_s_a : torch.Tensor # keep the logits of the node's children
+    visits: int
+    children: list['Node']
 
-	def __init__(self, current_token='', value=0, visits=0):
-		self.current_token = token
-		self.visits = visits
-		self.Q_s_a = {}
-		self.P_UCB_s_a = {}
-		self.children = []
+    def __init__(self, current_token, string, visits=0):
+        self.current_token = current_token
+        self.string = string
+        self.visits = visits
+        self.Q_s_a = None
+        self.P_UCB_s_a = None
+        self.P_s_a = None
+        self.children = []
 
 
 # Note that if we call expand and evaluate_full_paths from select, i.e. fold it in
@@ -52,50 +53,50 @@ class Node:
 TODO5(annhe): This should be unit tested.
 """
 def select(root:Node, ) -> Node:
-	"""
-	Given the root node v_o of the MCTS tree,
-	using P-UCB as a criterion, recursively
-	traverse the tree to find a node that has not been
-	previously expanded (a node without children nodes).
+    """
+    Given the root node v_o of the MCTS tree,
+    using P-UCB as a criterion, recursively
+    traverse the tree to find a node that has not been
+    previously expanded (a node without children nodes).
 
 
-	Args: root, the root node of the MCTS tree
-	Returns: 
-		root: the newly added node to the tree, v_n
-		path_nodes: the list of node.str in the tree which were traversed, starting
-		from the root node v_o, i.e. [v_o.str, v_1.str,...,v_n.str]
+    Args: root, the root node of the MCTS tree
+    Returns: 
+        root: the newly added node to the tree, v_n
+        path_nodes: the list of node.str in the tree which were traversed, starting
+        from the root node v_o, i.e. [v_o.str, v_1.str,...,v_n.str]
 
-	(todo): I'm thinking that instead of the node, which is copied,
-	we should return node.str, which allows us to traverse the path of the 
-	real MCTS tree's data structure in backpropagate_statistics, since Q_s_a 
-	is indexed by string
-	"""
-	mcts_tree_root = root # save this
-	path_nodes = []
-	path_nodes.append(root.str) # name of the current node
-	while len(root.children) > 0:
-		UCB_values = [P_UCB_s_a[child.str] for child in root.children]
-		arg_max, max_UCB = max(list(enumerate(UCB_values)), key=lambda x: x) 
-		root = root.children[arg_max]
-		path_nodes.append(root.str)
+    (todo): I'm thinking that instead of the node, which is copied,
+    we should return node.str, which allows us to traverse the path of the 
+    real MCTS tree's data structure in backpropagate_statistics, since Q_s_a 
+    is indexed by string
+    """
+    mcts_tree_root = root # save this
+    path_nodes = []
+    path_nodes.append(root.str) # name of the current node
+    while len(root.children) > 0:
+        UCB_values = [P_UCB_s_a[child.str] for child in root.children]
+        arg_max, max_UCB = max(list(enumerate(UCB_values)), key=lambda x: x) 
+        root = root.children[arg_max]
+        path_nodes.append(root.str)
 
-	# return root, path_nodes
-	"""
-	call expand and evaluate
-	"""
-	# root now points to the root v_o to be expanded
-	beams_list = expand(root)
-	max_rollout_reward, top_action, top_program = evaluate_full_paths(beams_list)
+    # return root, path_nodes
+    """
+    call expand and evaluate
+    """
+    # root now points to the root v_o to be expanded
+    beams_list = expand(root)
+    max_rollout_reward, top_action, top_program = evaluate_full_paths(beams_list)
 
-	# add the new best action to the Tree
-	new_node = Node(string=top_action)
-	# add the best action to path_nodes
-	path_nodes.append(top_action)
+    # add the new best action to the Tree
+    new_node = Node(string=top_action)
+    # add the best action to path_nodes
+    path_nodes.append(top_action)
 
-	return top_program, max_rollout_reward, path_nodes
+    return top_program, max_rollout_reward, path_nodes
 
 
-	
+    
 
 """
 TODO6(annhe) 
@@ -104,17 +105,17 @@ This was tested in the notebook.
 This should be unit tested in _test.py file.
 """
 def logits_to_token_strings(logits):
-	"""
-	Helper function, computes tokens and str representations given logit representation
+    """
+    Helper function, computes tokens and str representations given logit representation
 
-	Args:
-		logits: (batch_size, vocab_size)
-	Returns:
-		tokens: list[Torch.tensor] of tokens of length batch_size
-		str: a list of length batch_size
-	"""
-	k = logits.shape[0]
-	next_tokens = torch.log_softmax(beam_output.logits[0], dim=1)
+    Args:
+        logits: (batch_size, vocab_size)
+    Returns:
+        tokens: list[Torch.tensor] of tokens of length batch_size
+        str: a list of length batch_size
+    """
+    k = logits.shape[0]
+    next_tokens = torch.log_softmax(beam_output.logits[0], dim=1)
     next_tokens = torch.argmax(next_tokens, dim=1)
     str_repr = tokenizer.batch_decode(next_tokens)
     next_tokens = list(torch.chunk(next_tokens,chunks=k,dim=0))
@@ -129,12 +130,12 @@ But it should still be tested, first in a notebook,
 Then a unit test
 """
 def expand(root:Node, tokenizer, model, k, max_beam_len):
-	"""
-	Note that we can do a more fine-grained exploration of expand by interpolating
-	between the expansion and the roll-out, both of which are done with top-k decoding / beam search.
-	"""
-	# get top k - one of these will become the best action, and we will need to keep track of that token
-	beam_output = model.generate(
+    """
+    Note that we can do a more fine-grained exploration of expand by interpolating
+    between the expansion and the roll-out, both of which are done with top-k decoding / beam search.
+    """
+    # get top k - one of these will become the best action, and we will need to keep track of that token
+    beam_output = model.generate(
     root.tokens,
     max_new_tokens=1,
     num_beams=k,
@@ -149,45 +150,45 @@ def expand(root:Node, tokenizer, model, k, max_beam_len):
     # in the sequence, i.e. log p(y1y2...yk), which decomposes sum-wise
     root.P_s_a = beam_output.sequences_scores # size 
 
-   	# Note: these are the candidates for v_n
-   	next_tokens, str_repr = logits_to_token_strings(beam_output.logits[0])
+    # Note: these are the candidates for v_n
+    next_tokens, str_repr = logits_to_token_strings(beam_output.logits[0])
 
-   	# you'll have to add the same decoding code here
+    # you'll have to add the same decoding code here
     scores = list(torch.chunk(beam_output.sequences_scores,chunks=k,dim=0))
     beams = list(torch.chunk(beam_output.sequences,chunks=k,dim=0))
     beam_list = [(a,b,c,d) for a,b,c,d in zip(scores,beams,str_repr,next_tokens)]
 
     for _ in range(max_beam_len-1):
-    	new_list = []
-    	for current_path in beam_list:
-    		"""
-    		https://huggingface.co/docs/transformers/v4.43.3/en/main_classes/text_generation#transformers.GenerationMixin.generate
+        new_list = []
+        for current_path in beam_list:
+            """
+            https://huggingface.co/docs/transformers/v4.43.3/en/main_classes/text_generation#transformers.GenerationMixin.generate
 
-    		Q: what is the output of model.generate?
-    		
-    		It returns ModelOutput, i.e. GenerateBeamEncoderDecoderOutput
-    		https://huggingface.co/docs/transformers/v4.43.3/en/internal/generation_utils#transformers.generation.GenerateBeamDecoderOnlyOutput
+            Q: what is the output of model.generate?
+            
+            It returns ModelOutput, i.e. GenerateBeamEncoderDecoderOutput
+            https://huggingface.co/docs/transformers/v4.43.3/en/internal/generation_utils#transformers.generation.GenerateBeamDecoderOnlyOutput
 
-    		"""
-    		beam_output = model.generate(
-    		current_path,
-    		max_new_tokens=1,
-    		num_beams=k,
-    		num_return_sequences=k,
-    		return_dict_in_generate=True,
-    		output_logits=True,
-    		output_scores=True,
-    		early_stopping=True)
+            """
+            beam_output = model.generate(
+            current_path,
+            max_new_tokens=1,
+            num_beams=k,
+            num_return_sequences=k,
+            return_dict_in_generate=True,
+            output_logits=True,
+            output_scores=True,
+            early_stopping=True)
 
-    		next_tokens, str_repr = logits_to_token_strings(beam_output.logits[0])
+            next_tokens, str_repr = logits_to_token_strings(beam_output.logits[0])
 
-    		current_beams = list(torch.chunk(beam_output.sequences,chunks=k,dim=0))
-    		scores = list(torch.chunk(beam_output.sequences_scores,chunks=k,dim=0))
-    		current_beam_list = [(a,b,c,d) for a,b,c,d in zip(scores,current_beams,str_repr,next_tokens)]
+            current_beams = list(torch.chunk(beam_output.sequences,chunks=k,dim=0))
+            scores = list(torch.chunk(beam_output.sequences_scores,chunks=k,dim=0))
+            current_beam_list = [(a,b,c,d) for a,b,c,d in zip(scores,current_beams,str_repr,next_tokens)]
 
-    		new_list.extend(current_beam_list)
+            new_list.extend(current_beam_list)
 
-    	beam_list = new_list
+        beam_list = new_list
     # should be list of k^(max_beam_len) full paths
     return beam_list 
 
@@ -213,52 +214,53 @@ when the MCTS algorithm was called
 TODO3(annhe): This should be unit tested.
 """
 def evaluate_full_paths(beam_list: list[tuple[torch.Tensor, torch.Tensor]]): 
-	# returns full decoded path and its score
-	# consider using a different reward model
-	# suggested in https://openreview.net/pdf?id=Lr8cOOtYbfL to use max(score)
+    # returns full decoded path and its score
+    # consider using a different reward model
+    # suggested in https://openreview.net/pdf?id=Lr8cOOtYbfL to use max(score)
 
-	"""
-	Args:
-		beams_list: list of candidate beams
+    """
+    Args:
+        beams_list: list of candidate beams
 
-	Returns:
-		max_rollout_reward: the reward from the full sequence
-		top_action: top next action as a string, i.e. v_n.str, the first string in the list
-		top_program: the entire sequence representing the full program
-	"""
-	res = sorted(beam_list,key=lambda x: x[1], reverse=True)[0]
-	# scores,current_beams,str_repr,next_tokens
-	program = [' '.join([r[2] for r in res])]
-	return res[0], res[2][0], program
+    Returns:
+        max_rollout_reward: the reward from the full sequence
+        top_action: top next action as a string, i.e. v_n.str, the first string in the list
+        top_program: the entire sequence representing the full program
+    """
+    res = sorted(beam_list,key=lambda x: x[1], reverse=True)[0]
+    # scores,current_beams,str_repr,next_tokens
+    program = [' '.join([r[2] for r in res])]
+    return res[0], res[2][0], program
 
 
 """
 TODO1(annhe): This should be rewritten
 """
-def backpropagate_statistics_V1(path_nodes, max_rollout_reward, c_base, c):
-	# 1. add 1 to all state visit counts
-	# 2. recalculate P_UCB_s_a recursively (backwards)
-	# 3. update Q_s_a with max_rollout_reward
-	# According to the paper, this is
-	# Q(s'',a'') <-- max(Q(s'',a''),r)
-	# P(a|s) given by last log softmax in the return value
-	
-	# We need to update path_nodes in reverse order
-	reversed_path_nodes = list(reversed(path_nodes))
-	for i, node in enumerate(reversed_path_nodes):
-		node.visits += 1
-		# Q(s'',a'') <-- max(Q(s'',a''),r)
-		new_q_s_a = {k: max(v, max_rollout_reward) for k,v in node.Q_s_a.items()}
-		node.Q_s_a = new_q_s_a
-		# Recalculated P_UCB_s_a
-		beta = math.log((node.visits + c_base + 1) / c_base) + c
-		if i > 0:
-			s_prime_visits = reversed_path_nodes[i-1].visits 
-		else:
-			s_prime_visits = 0
-		for i, (k, v) in enumerate(new_q_s_a.items()):
-    		print(i, k, v)
-    		node.P_UCB_s_a[k] = v + beta * node.P_s_a[i] * math.sqrt(torch.log(node.visits)) / (1 + s_prime_visits)
+def backpropagate_statistics(path_nodes, max_rollout_reward, c_base, c):
+    # 1. add 1 to all state visit counts
+    # 2. recalculate P_UCB_s_a recursively (backwards)
+    # 3. update Q_s_a with max_rollout_reward
+    # According to the paper, this is
+    # Q(s'',a'') <-- max(Q(s'',a''),r)
+    # P(a|s) given by last log softmax in the return value
+    
+    # We need to update path_nodes in reverse order
+    reversed_path_nodes = list(reversed(path_nodes))
+    for i, node in enumerate(reversed_path_nodes):
+        node.visits += 1
+        # Q(s'',a'') <-- max(Q(s'',a''),r)
+        if node.Q_s_a is not None: # assume node.Q_s_a is none for the newest node v_n
+            new_q_s_a = {k: max(v, max_rollout_reward) for k,v in node.Q_s_a.items()}
+            node.Q_s_a = new_q_s_a
+            # Recalculated P_UCB_s_a
+            beta = math.log((node.visits + c_base + 1) / c_base) + c
+            if i > 0:
+                s_prime_visits = reversed_path_nodes[i-1].visits 
+            else:
+                s_prime_visits = 0
+            for i, (k, v) in enumerate(new_q_s_a.items()):
+                print(i, k, v)
+                node.P_UCB_s_a[k] = v + beta * node.P_s_a[i] * math.sqrt(torch.log(node.visits)) / (1 + s_prime_visits)
 
 
 
@@ -267,20 +269,20 @@ def backpropagate_statistics_V1(path_nodes, max_rollout_reward, c_base, c):
 TODO2(annhe): This should be rewritten / fleshed out
 """
 def main_algorithm(prompt, max_rollouts) -> str:
-	program_dictionary = dict() # to store fully generated programs
-	# program_ditionary[program] = rollout_reward
-	
-	# TODO(annhe): create starting tring
-	root_node = Node(prompt)
+    program_dictionary = dict() # to store fully generated programs
+    # program_ditionary[program] = rollout_reward
+    
+    # TODO(annhe): create starting tring
+    root_node = Node(prompt)
 
-	for _ in max_rollouts:
-		top_program, max_rollout_reward, path_nodes = select(node)
-		program_dictionary[top_program] = max_rollout_reward
-		backpropagate_statistics(path_nodes, max_rollout_reward) #todo
+    for _ in max_rollouts:
+        top_program, max_rollout_reward, path_nodes = select(node)
+        program_dictionary[top_program] = max_rollout_reward
+        backpropagate_statistics(path_nodes, max_rollout_reward) #todo
 
-	v = list(program_dictionary.values())
-	k = list(program_dictionary.keys())
-	return k[v.index(max(v))] # return the program with the max rollout reward
+    v = list(program_dictionary.values())
+    k = list(program_dictionary.keys())
+    return k[v.index(max(v))] # return the program with the max rollout reward
 
 
 
--- ai_planning_searching/mcts_utils_tests.py
+++ ai_planning_searching/mcts_utils_tests.py
@@ -3,28 +3,44 @@ import torch
 import collections
 import json
 from transformers import pipeline, set_seed
+import unittest
+from mcts_utils import Node
 
 
-def test_logits_to_token_strings():
-	pass
 
+class testMCTSUtils(unittest.TestCase):
 
-def test_select():
-	pass
+	@classmethod
+	def setUpClass(cls):
+		pass 
 
+	@classmethod
+	def tearDownClass(cls):
+		pass
 
-def test_expand():
-	pass
+	def testNode(self):
+		# test that we can create a basic node
+		node_token = torch.Tensor([2])
+		string = 'hello'
+		new_node = Node(node_token, string)
+		self.assertEqual(new_node.string, 'hello')
+		self.assertEqual(new_node.current_token, torch.Tensor([2]))
 
+	def test_logits_to_token_strings(self):
+		pass
 
-def test_evaluate_full_paths():
-	pass 
+	def test_select(self):
+		pass
 
+	def test_expand(self):
+		pass
 
-def backpropagate_statistics():
-	pass
+	def test_evaluate_full_paths(self):
+		pass 
 
+	def backpropagate_statistics(self):
+		pass
 
-def test_main_algorthim():
-	pass
+	def test_main_algorthim(self):
+		pass
 
