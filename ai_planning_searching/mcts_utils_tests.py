fix issue with test where backprop_statistics previously did not populate Q_s_a

ai_planning_searching/mcts_utils_tests.py
@@ -28,6 +28,13 @@ class testMCTSUtils(unittest.TestCase):
         node_3.P_UCB_s_a['4'] = 5
         node_4 = Node(current_token=torch.Tensor([4]), string='4')
 
+        node_0.children = [node_1, node_3]
+        node_1.children = [node_2]
+        node_3.children = [node_4]
+        # are these going to be persistent on the stack?
+        # I think you're going to need to write a different function
+        # to instantiate them
+
         self.mcts_root_node = node_0
 
 
@@ -70,6 +77,10 @@ class testMCTSUtils(unittest.TestCase):
         node_4.P_s_a = torch.ones((beam_width,),dtype=torch.float)
         node_4.P_s_a = node_4.P_s_a / beam_width
         node_dictionary['4'] = node_4
+
+        node_0.children = [node_1, node_3]
+        node_1.children = [node_2]
+        node_3.children = [node_4]
 
         self.mcts_root_node = node_0
         return node_dictionary
@@ -161,7 +172,53 @@ class testMCTSUtils(unittest.TestCase):
         path_nodes = ['0', '3', '4']
         path_strings = ['0', '3', '4']
         max_rollout_reward = 5
-        node_dictionary = self.create_mock_tree_2()
+        #node_dictionary = self.create_mock_tree_2()
+
+        #### Testing this instead ####
+        beam_width = 5
+        node_dictionary = dict()
+
+
+        node_0 = Node(current_token=torch.Tensor([0]), string='0')
+        node_0.P_UCB_s_a['1'] = 0
+        node_0.P_UCB_s_a['3'] = 0
+        node_0.P_s_a = torch.ones((beam_width,),dtype=torch.float)
+        node_0.P_s_a = node_0.P_s_a / beam_width
+        node_dictionary['0'] = node_0
+
+        node_1 = Node(current_token=torch.Tensor([1]), string='1')
+        node_1.P_UCB_s_a['2'] = 0
+        node_1.P_s_a = torch.ones((beam_width,),dtype=torch.float)
+        node_1.P_s_a = node_1.P_s_a / beam_width
+        node_dictionary['1'] = node_1
+
+        node_2 = Node(current_token=torch.Tensor([2]), string='2')
+        node_2.P_s_a = torch.ones((beam_width,),dtype=torch.float)
+        node_2.P_s_a = node_2.P_s_a / beam_width
+        node_dictionary['2'] = node_2
+
+
+        node_3 = Node(current_token=torch.Tensor([3]), string='3')
+        node_3.P_UCB_s_a['4'] = 5
+        node_3.P_s_a = torch.ones((beam_width,),dtype=torch.float)
+        node_3.P_s_a = node_3.P_s_a / beam_width
+        node_dictionary['3'] = node_3
+
+        node_4 = Node(current_token=torch.Tensor([4]), string='4')
+        node_4.P_s_a = torch.ones((beam_width,),dtype=torch.float)
+        node_4.P_s_a = node_4.P_s_a / beam_width
+        node_dictionary['4'] = node_4
+
+        node_0.children = [node_1, node_3]
+        node_1.children = [node_2]
+        node_3.children = [node_4]
+
+
+        ##############################
+
+
+
+
         backpropagate_statistics(path_nodes, path_strings, max_rollout_reward, c_base, c, node_dictionary)
         # test that all of the Q_s_a values of the nodes on the path are updated
         # with max_rollout_reward