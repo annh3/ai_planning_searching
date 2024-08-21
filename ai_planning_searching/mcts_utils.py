fixed backpropagate_statistics

ai_planning_searching/mcts_utils.py
@@ -15,7 +15,7 @@ class Node:
     string: str # string representation of current token
     Q_s_a: dict[str,float] # Q values of the node's children
     P_UCB_s_a: dict[str, float] # P-UCB values of the node's children
-    P_s_a : torch.Tensor # keep the logits of the node's children
+    P_s_a : dict[str, float] # probability of the node's children
     visits: int
     children: list['Node']
 
@@ -25,7 +25,7 @@ class Node:
         self.visits = visits
         self.Q_s_a = dict()
         self.P_UCB_s_a = dict()
-        self.P_s_a = None
+        self.P_s_a = dict()
         self.children = []
 
     # omg trying this out
@@ -67,12 +67,12 @@ def select(root:Node, node_dictionary: dict[str, Node]) -> tuple[Node, list[str]
     """
     counter = 0
     mcts_tree_root = root # save this
-    node_dictionary[mcts_tree_root.string + '_' + str(counter)] = mcts_tree_root
     path_nodes = []
     path_strings = []
     print("root.string: ", root.string)
     root_node_name = root.string + '_' + str(counter)
     path_nodes.append(root_node_name) # name of the current node
+    node_dictionary[root_node_name] = mcts_tree_root
     path_strings.append(root.string)
     
 
@@ -81,7 +81,7 @@ def select(root:Node, node_dictionary: dict[str, Node]) -> tuple[Node, list[str]
         UCB_values = [root.P_UCB_s_a[child.string] for child in root.children]
         arg_max, max_UCB = max(list(enumerate(UCB_values)), key=lambda x: x) 
         root = root.children[arg_max]
-        node_dictionary[root.string] = root
+        node_dictionary[root.string + '_' + str(counter)] = root
         path_nodes.append(root.string + '_' + str(counter))
         path_strings.append(root.string)
 
@@ -114,8 +114,8 @@ def expand(root:Node, tokenizer, model, k, max_beam_len):
     """
     Each item of beam is a tuple like
 
-    beam_item = (sequence_score, partial_tokens, partial_program)
-    ...       = (torch.Tensor([0.5]), ['hello', 'world', ..., '!'], [torch.Tensor([1]),...,torch.Tensor([12])])
+    beam_item = (first_transition_score, sequence_score, partial_tokens, partial_program)
+    ...       = (torch.Tensor([0.18]), torch.Tensor([0.5]), ['hello', 'world', ..., '!'], [torch.Tensor([1]),...,torch.Tensor([12])])
     """
     # get top k - one of these will become the best action, and we will need to keep track of that token
     # pdb.set_trace()
@@ -131,22 +131,23 @@ def expand(root:Node, tokenizer, model, k, max_beam_len):
     output_logits=True)
 
     # pdb.set_trace()
+    transition_scores = list(torch.chunk(beam_output.scores[0],chunks=k,dim=0))
+    transition_scores = [t[-1] for t in transition_scores]
+    # keep the score of this first expand only
 
     # todo(annhe): Add scores of beams to node.P_s_a
     # https://discuss.huggingface.co/t/announcement-generation-get-probabilities-for-generated-output/30075/14
     # sequences_scores are the sum of the log probabilities of the generated tokens
     # in the sequence, i.e. log p(y1y2...yk), which decomposes sum-wise
-    root.P_s_a = beam_output.sequences_scores # size 
 
     # Note: these are the candidates for v_n
     next_tokens, str_repr = logits_to_token_strings(beam_output.logits[0], tokenizer)
-    ### !!! I think here you need to concatenate the next_tokens with the previous tokens
 
     # you'll have to add the same decoding code here
-    scores = list(torch.chunk(beam_output.sequences_scores,chunks=k,dim=0))
+    sequence_scores = list(torch.chunk(beam_output.sequences_scores,chunks=k,dim=0))
 
 
-    beam_list = [(a,[b],[c]) for a,b,c in zip(scores, next_tokens, str_repr)]
+    beam_list = [(t,a,[b],[c]) for t,a,b,c in zip(transition_scores, sequence_scores, next_tokens, str_repr)]
 
     for _ in range(max_beam_len-1):
         print("Length of beam_list: ", len(beam_list))
@@ -162,7 +163,7 @@ def expand(root:Node, tokenizer, model, k, max_beam_len):
 
             """
             # pdb.set_trace()
-            current_tokens = torch.cat(current_path[1])
+            current_tokens = torch.cat(current_path[2])
             current_tokens = current_tokens.unsqueeze(0)
             # pdb.set_trace()
             beam_output = model.generate(
@@ -177,10 +178,10 @@ def expand(root:Node, tokenizer, model, k, max_beam_len):
 
             next_tokens, str_repr = logits_to_token_strings(beam_output.logits[0], tokenizer)
 
-            scores = list(torch.chunk(beam_output.sequences_scores,chunks=k,dim=0))
-            for score,string,next_token in zip(scores,str_repr,next_tokens):
+            sequence_scores = list(torch.chunk(beam_output.sequences_scores,chunks=k,dim=0))
+            for score,string,next_token in zip(sequence_scores,str_repr,next_tokens):
                 # add to beams
-                cur = (score,current_path[1]+[next_token],current_path[2]+[string])
+                cur = (current_path[0],score,current_path[2]+[next_token],current_path[3]+[string])
                 new_list.append(cur)
 
         beam_list = new_list
@@ -202,10 +203,10 @@ def evaluate_full_paths(beam_list):
         top_action: top next action as a string, i.e. v_n.str, the first string in the list
         top_program: the entire sequence representing the full program
     """
-    res = sorted(beam_list,key=lambda x: x[0], reverse=True)[0]
+    res = sorted(beam_list,key=lambda x: x[1], reverse=True)[0]
     # returns score as a torch.Tensor float, the first string representing
     # the next action from MCTS search, and the program as a list of torch.Tensor tokens
-    return res[0], res[2][0], res[1]
+    return res[1], res[3][1], res[2], res[0]
 
 
 # v_n is the newest node, we need to skip q_a as v_n does not have a child node
@@ -221,12 +222,14 @@ def backpropagate_statistics(path_nodes, path_strings, max_rollout_reward, c_bas
     reversed_path_nodes = list(reversed(path_nodes))
     list_len = len(reversed_path_nodes)
     path_strings = list(reversed(path_strings))
-    pdb.set_trace()
 
     # we need to keep a dictionary from node string names to Nodes
     for i, node_name in enumerate(reversed_path_nodes):
         if i == 0:
+            node = node_dictionary[node_name]
+            node.visits += 1
             continue
+        # pdb.set_trace()
         node = node_dictionary[node_name]
         node.visits += 1
         # Q(s'',a'') <-- max(Q(s'',a''),r)
@@ -234,19 +237,17 @@ def backpropagate_statistics(path_nodes, path_strings, max_rollout_reward, c_bas
             # create an entry, how do you know what the next action is?
             # it's path_strings[i-1]
             node.Q_s_a[path_strings[i-1]] = max_rollout_reward
-            node.P_UCB_s_a[path_strings[i-1]] = max_rollout_reward
+            s_prime_visits = node_dictionary[reversed_path_nodes[i-1]].visits 
+            node.P_UCB_s_a[path_strings[i-1]] = max_rollout_reward + node.P_s_a[path_strings[i-1]] * math.sqrt(math.log(node.visits)) / (1 + s_prime_visits)
         else: # assume node.Q_s_a is none for the newest node v_n
             new_q_s_a = {k: max(v, max_rollout_reward) for k,v in node.Q_s_a.items()}
             node.Q_s_a = new_q_s_a
             # Recalculated P_UCB_s_a
             beta = math.log((node.visits + c_base + 1) / c_base) + c
-            if i > 0:
-                s_prime_visits = node_dictionary[reversed_path_nodes[i-1]].visits 
-            else:
-                s_prime_visits = 0
+            s_prime_visits = node_dictionary[reversed_path_nodes[i-1]].visits 
             for i, (k, v) in enumerate(new_q_s_a.items()):
                 print(i, k, v)
-                node.P_UCB_s_a[k] = v + beta * node.P_s_a[i] * math.sqrt(torch.log(node.visits)) / (1 + s_prime_visits)
+                node.P_UCB_s_a[k] = v + beta * node.P_s_a[k] * math.sqrt(math.log(node.visits)) / (1 + s_prime_visits)
 
 """
 TODO(annhe): write tests for this
@@ -271,18 +272,19 @@ def main_algorithm(prompt, max_rollouts, k, max_beam_len) -> str:
    
 
 
-    for _ in range(max_rollouts):
+    for rollout_i in range(max_rollouts):
+        print("rollout_i: ", rollout_i)
         node_dictionary = dict()
         node_to_expand, path_nodes, counter, path_strings = select(root_node, node_dictionary)
         beams_list = expand(node_to_expand, tokenizer, model, k, max_beam_len)
-        max_rollout_reward, top_action, top_program = evaluate_full_paths(beams_list)
+        max_rollout_reward, top_action, top_program, top_action_proba = evaluate_full_paths(beams_list)
         top_program_tensor = torch.cat(top_program)
         program_dictionary[top_program_tensor] = max_rollout_reward
         counter += 1
         new_node_name = top_action + '_' + str(counter)
         # current_token is top_action encoded
         action_token = tokenizer.encode(top_action)
-        action_token = torch.Tensor(top_action)
+        action_token = torch.Tensor(action_token)
         action_token = prompt_tokens.type(torch.LongTensor)
         new_node = Node(current_token=action_token,string=top_action)
         # add the best action to path_nodes
@@ -290,8 +292,13 @@ def main_algorithm(prompt, max_rollouts, k, max_beam_len) -> str:
         path_strings.append(top_action)
         node_dictionary[new_node_name] = new_node
         # you need to add the child node to the previous node
+        node_to_expand.P_s_a[top_action] = top_action_proba
         node_to_expand.children.append(new_node)
+        print("path_nodes: ", path_nodes)
+        print("path_strings: ", path_strings)
+        print("node_dictionary keys: ", node_dictionary.keys())
         backpropagate_statistics(path_nodes, path_strings, max_rollout_reward,  c_base, c, node_dictionary) #todo
+        print("\n\n")
 
     v = list(program_dictionary.values())
     k = list(program_dictionary.keys())