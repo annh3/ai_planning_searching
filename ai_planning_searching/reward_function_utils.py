d8e34e9f776b4e58a9ba6967c2ddc510fa54714b
Ann He <annxhe@gmail.com>
2024-08-29 10:18:32 -0500

    save state

--- ai_planning_searching/reward_function_utils.py
+++ ai_planning_searching/reward_function_utils.py
@@ -1,4 +1,16 @@
 """
+TODO(annhe): 
+
+PAUSE on the design of these utils as we have not yet decided
+whether to try finetuning on the APPS dataset at all.
+
+If we decide to finetune on the APPS dataset we will need to match
+the formatting of that dataset.
+
+O.W., can try to curate our own dataset.
+
+
+
 This file loads the APPS dataset.
 
 Dataset Difficulty
@@ -26,9 +38,12 @@ Metrics
 """
 from datasets import load_dataset
 import json
+import pdb
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast # TODO(annhe): is there a generic form of this?
 
 
+#TODO(annhe): rewrite this so that unit_test_questions, unit_test_sols
+# is returned as list of integers, list of integers
 def problem_unit_tests(ds):
 	"""
 	Wraps around dataset
@@ -38,13 +53,15 @@ def problem_unit_tests(ds):
 	return ((example["question"], example["solutions"], json.loads(example["input_output"])["inputs"], json.loads(example["input_output"])["outputs"]) for example in ds)
 
 
+def run_single_test_case():
+	pass
+
 
 def run_apps_evals(candidate_program, data):
 	"""
 	Args:
 		candidate_program: python program in the form of a string
-		unit_test_inputs: list[str] of inputs
-		unit_test_outputs: list[str] of outputs
+		data: list[list[str], list[str]] of inputs and outputs, or similar iterable
 
 
 	Returns:
@@ -52,11 +69,14 @@ def run_apps_evals(candidate_program, data):
 	"""
 	tests_passed = 0
 	total_tests = 0
+	# pdb.set_trace()
 
-	for question, _, unit_test_questions, unit_test_solutions in data:
-		for x,y in zip(unit_test_questions, unit_test_solutions):
-			fn_output = exec(candidate_program)
-			if y == fn_output:
+	for _, _, unit_test_questions, unit_test_solutions in data:
+		for input_value, gt_output_value in zip(unit_test_questions, unit_test_solutions):
+			loc = {}
+			exec(candidate_program, globals(), loc)
+			fn_output = loc['output_value']
+			if gt_output_value == fn_output:
 				tests_passed += 1
 			total_tests += 1
 	return float(tests_passed) / total_tests
--- ai_planning_searching/reward_function_utils_tests.py
+++ ai_planning_searching/reward_function_utils_tests.py
@@ -10,41 +10,6 @@ from reward_function_utils import load_apps, problem_unit_tests, run_apps_evals
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast 
 
 
-
-str_1 = json.dumps({ "inputs": [ "4\n4\n0001\n1000\n0011\n0111\n3\n010\n101\n0\n2\n00000\n00001\n4\n01\n001\n0001\n00001\n" ], "outputs": [ "1\n3 \n-1\n0\n\n2\n1 2 \n" ] })
-str_2 = json.dumps({ "inputs": [ "9\n1 10 7 1\n3 3 3 0\n8 2 10 4\n8 2 10 100\n-10 20 -17 2\n-3 2 2 0\n-3 1 2 0\n2 3 2 3\n-1 3 -2 2\n" ], "outputs": [ "7\n0\n4\n0\n30\n5\n4\n0\n3\n" ] })
-
-
-def pretend_dataset_list():
-    pretend_dataset_list = [
-    {"question": "question_1", "solutions": "solution_1", "input_output": str_1},
-    {"question": "question_2", "solutions": "solution_2", "input_output": str_2}]
-
-    n = len(pretend_dataset_list)
-    i = 0
-    while i < n:
-        yield pretend_dataset_list[i]
-        i += 1
-
-
-class PretendDataset:
-    def __init__(self, lst):
-        self.lst = lst
-        self.counter = 0
-        self.max = len(self.lst)
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.counter < self.max:
-            return self.lst[self.n]
-            self.counter += 1
-        else:
-            raise StopIteration
-
-
-
 class testMCTSUtils(unittest.TestCase):
 
     def setUp(self):
@@ -78,25 +43,28 @@ class testMCTSUtils(unittest.TestCase):
     ['cher', 'melo']
     """
     def test_exec_program(self):
-        x = 5
-        # assume program is the return of the lm_call
-        program = """def exponentiate(x):
-            return x**2""" 
-        res = exec(program)
+        program = """
+x = 5
+
+return_val = x**2"""
+        loc = {}
+        exec(program, globals(), loc)
+        res = loc['return_val']
         self.assertEqual(res, 25)
 
 
     def test_run_apps_evals(self):
-        ds_iterator = pretend_dataset_list()
-        data = problem_unit_tests(ds_iterator)
-        program = """def exponentiate(x):
-            return x**2""" 
+        data = [["question", "solution_program", [5],[25]]]
+        program = f"""
+        x = {input_value}
+
+        output_value = x**2"""
         pass_rate = run_apps_evals(program, data)
         self.assertIsInstance(pass_rate, float)
 
-
+"""
     def test_problem_unit_tests(self):
-        ds_iterator = pretend_dataset_list()
+        #ds_iterator = pretend_dataset_list()
         data = problem_unit_tests(ds_iterator)
 
         for program, solution, unit_test_inputs, unit_test_outputs in data:
@@ -104,3 +72,4 @@ class testMCTSUtils(unittest.TestCase):
             self.assertIsInstance(solution, str)
             self.assertIsInstance(unit_test_inputs, list)
             self.assertIsInstance(unit_test_outputs, list)
+"""
